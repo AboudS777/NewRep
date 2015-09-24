7182e033866fac761ba16bcc5b078e6656b97470
abdallahsaket3 <abdallahsaket3@5010-040.ODENG2.CARLETON.CA>
2015-09-24 12:31:47 -0400

    test

---
+++ AddressBook2/src/AddressBook2.java
@@ -0,0 +1,32 @@
+
+
+import java.util.*;
+
+public class AddressBook2 {
+	
+	private List<buddyinfo2> contacts;
+
+	public AddressBook2(){
+		contacts = new ArrayList<buddyinfo2>();
+	}
+
+	public void addContact(buddyinfo2 buddy){
+		contacts.add(buddy);
+	}
+
+	public buddyinfo2 removeBuddy(int index){
+		if(index >= 0 && index < this.contacts.size()){
+			return contacts.remove(index);
+		}
+		return null;
+	}
+	
+	public static void main(String aargs[]){
+		buddyinfo2 buddy = new buddyinfo2("Tom", "carleton", 123412);
+		AddressBook2 addressBook = new AddressBook2();
+		addressBook.addContact(buddy);
+		addressBook.removeBuddy(0);
+	}
+	
+
+}
